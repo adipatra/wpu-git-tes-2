0313d2b55113c60b7d71af551c8e1b7261275205
adipatra <febriyan.dbrilianz@gmail.com>
2021-06-03 14:09:39 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Coba Git Remote</title>
+    <link rel="stylesheet" href="stylesheet.css">
+  </head>
+  <body>
+
+        <h1>HELLO WORLD</h1>
+        <h2>Ayo belajar coding</h2>
+     
+  </body>
+</html>
